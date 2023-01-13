d18665ac3e5d58a538da2daf720de4c08b47ceec
馬場義弘 <babayoshihiro@YB-MBP14.local>
2023-01-13 14:11:46 +0900

    your-coding ver.1.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,54 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  mode: 'development', //デフォルト
+  devtool: 'source-map',
   mode: "development",
   entry: "./src/js/script.js",
   output: {
     path: path.resolve(__dirname, "./dist"),
-    filename:'js/script.js',
+    filename: "js/script.js",
+    publicPath: '/',
   },
   module: {
     rules: [
       {
-        test: /\.css/,
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [["@babel/preset-env", { targets: "> 30%, not dead" }]], //セットになっている、targetsでブラウザーを指定できる。
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(css|sass|scss)/,
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
           },
           {
-            loader: 'css-loader',
+            loader: "css-loader",
+            options: {
+              sourceMap: false, //デバックする時にtrueにするとよい。
+            },
+          },
+          {
+            loader: "sass-loader",
           },
         ],
       },
       {
-        test: /\.(png|jpg)/,
-        type: 'asset/resource',
+        test: /\.(png|jpg|jpeg|svg|gif)/,
+        type: "asset/resource", 
         generator: {
-          filename: 'images/[name][ext]'
+          filename: "images/[name][ext]",
         },
         use: [
           // webpack4までの記述
@@ -38,17 +59,23 @@ module.exports = {
           //     name: 'images/',
           //   },
           // },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              //どのくらい圧縮するかを指定できる
+            },
+          },
         ],
       },
     ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: './css/style.css',
+      filename: "./css/style.css",
     }),
     new HtmlWebpackPlugin({
-        template: './src/templates/index.html',
+      template: "./src/templates/index.html",
     }),
     new CleanWebpackPlugin(),
-],
+  ],
 };
